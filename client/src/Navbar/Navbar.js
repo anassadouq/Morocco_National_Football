367baa050ef14d1981bcc5533237c2339978c15d
anas sadouq <sadouqanas1@gmail.com>
2024-03-04 23:09:53 +0100

    change class to className

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -11,27 +11,27 @@ export default function Navbar(){
     }
     
     return(
-        <nav class="navbar navbar-expand-lg container">
-            <div class="container-fluid">
+        <nav className="navbar navbar-expand-lg container">
+            <div className="container-fluid">
                 <Link to='/'>
                     <img src={"../images/logo.png"} width='40px'/>
                 </Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#"></a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#"></a>
                         </li>
-                        <li class="nav-item">
-                            <Link to='/' class="nav-link mx-1">Home</Link>
+                        <li className="nav-item">
+                            <Link to='/' className="nav-link mx-1">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to='/all_players' class="nav-link mx-1">All Players</Link>
+                        <li className="nav-item">
+                            <Link to='/all_players' className="nav-link mx-1">All Players</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to='/called_up' class="nav-link mx-1">Called-up</Link>
+                        <li className="nav-item">
+                            <Link to='/called_up' className="nav-link mx-1">Called-up</Link>
                         </li>
                     </ul>
                 </div>
@@ -42,7 +42,7 @@ export default function Navbar(){
                     
                 </ul>
                 <form className="d-flex">
-                    <button className='btn btn-dark' onClick={logout}><BiLogOut/></button>
+                    <button className='btn btn-dark' onClick={logout} style={{"width":"100%"}}><BiLogOut/></button>
                 </form>
             </div>
         </nav>
